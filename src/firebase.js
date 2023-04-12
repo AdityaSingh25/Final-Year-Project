09069278befaf329c181767b446fe6e8e6745cd3
AdityaSingh25 <richiaditya25@gmail.com>
2023-04-13 01:46:25 +0530

    Login done

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAygPQWptXGvqiGjq-n_UOKGPqQgFvQOfQ",
+  authDomain: "login-85aaf.firebaseapp.com",
+  projectId: "login-85aaf",
+  storageBucket: "login-85aaf.appspot.com",
+  messagingSenderId: "343629336768",
+  appId: "1:343629336768:web:5a71d6834d130819fcdd25",
+  measurementId: "G-4NSN10X67D",
+};
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+export { app, auth };
