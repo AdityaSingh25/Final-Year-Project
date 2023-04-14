5b51530d14bfab6d9c4769e9b8de673a35caec06
Aditya Narayan Singh <richiaditya25@gmail.com>
2023-04-14 13:03:28 +0530

    card section done

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -11,13 +11,13 @@ function Cards() {
             <CardItems
               src="images/img-9.jpg"
               text="Explore the hidden waterfall deep inside the Amazon Jungle"
-              label="Adventure"
+              
               path="/services"
             />
             <CardItems
-              src="images/img-2.jpg"
+              src="images/img-2.png"
               text="Travel through the Islands of Bali in a Private Cruise"
-              label="Luxury"
+              
               path="/services"
             />
           </ul>
@@ -25,19 +25,19 @@ function Cards() {
             <CardItems
               src="images/img-3.jpg"
               text="Set Sail in the Atlantic Ocean visiting Uncharted Waters"
-              label="Mystery"
+              
               path="/services"
             />
             <CardItems
               src="images/img-4.jpg"
               text="Experience Football on Top of the Himilayan Mountains"
-              label="Adventure"
+              
               path="/products"
             />
             <CardItems
               src="images/img-8.jpg"
               text="Ride through the Sahara Desert on a guided camel tour"
-              label="Adrenaline"
+              
               path="/sign-up"
             />
           </ul>
