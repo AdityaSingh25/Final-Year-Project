2fd80c555015ca526823d8c7e303da12b3208b34
Aditya Narayan Singh <richiaditya25@gmail.com>
2023-04-16 23:59:28 +0530

    Creating user

--- src/components/Herosection/Herosection.js
+++ src/components/Herosection/Herosection.js
@@ -7,23 +7,15 @@ function HeroSection() {
   return (
     <div className="hero-container">
       <video src="/videos/video-1.mp4" autoPlay loop muted />
-      <h1>FIRST TIME COURT CASE USING BLOCKCHAIN</h1>
-      <p>What are you waiting for?</p>
+      <h1>COURT CASE USING BLOCKCHAIN</h1>
+      
       <div className="hero-btns">
         <Button
           className="btns"
           buttonStyle="btn--outline"
           buttonSize="btn--large"
         >
-          GET STARTED
-        </Button>
-        <Button
-          className="btns"
-          buttonStyle="btn--primary"
-          buttonSize="btn--large"
-          onClick={console.log("hey")}
-        >
-          WATCH TRAILER <i className="far fa-play-circle" />
+          TRANSPERANCY
         </Button>
       </div>
     </div>
